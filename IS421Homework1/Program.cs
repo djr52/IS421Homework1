e01215037a210ab8863bcb89d8019072f7865303
Daniel Ramirez <juanfantasma1@yahoo.com>
2021-01-31 17:08:01 -0500

    added c# files

--- IS421Homework1/Program.cs
+++ IS421Homework1/Program.cs
@@ -6,7 +6,7 @@ namespace IS421Homework1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Daniel R.");
         }
     }
 }
